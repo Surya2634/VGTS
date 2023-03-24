829b1c575762fff2d7f92a6033e6376d64d83b8b
surya <suryasurya2634@gmail.com>
2023-03-24 13:34:27 +0530

    Added CSS

--- src/Page/MealDetail/index.js
+++ src/Page/MealDetail/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useLocation } from "react-router-dom";
+import styles from "./MealDetail.module.css";
 
 const MealDetail = () => {
   const location = useLocation();
@@ -15,9 +16,30 @@ const MealDetail = () => {
 
   return (
     <>
-      <img src={""} alt="Meal" />
-      <p>{location.state.id}</p>
-      <button onClick={navigateCheckout}>Check Out</button>
+      <h1>Meal Details</h1>
+      <div className={styles.MealDetailContainer}>
+        <div>
+          <img
+            src="https://www.themealdb.com/images/media/meals/tvvxpv1511191952.jpg"
+            alt="Meal"
+            className={styles.MealImg}
+          />
+        </div>
+        <div className={styles.MealDescription}>
+          {/* <p>{location.state.id}</p> */}
+          <h1>Dish Name</h1>
+          <p>Dish Category</p>
+          <p className={styles.mealInstruction}>
+            "In a large casserole, fry the sausages until brown all over – about
+            10 mins.\r\n\r\nAdd the tomato sauce, stirring well, then stir in
+            the beans, treacle or sugar and mustard. Bring to the simmer, cover
+            and cook for 30 mins. Great served with crusty bread or rice."
+          </p>
+          <button onClick={navigateCheckout} className={styles.ChekoutButton}>
+            Check Out
+          </button>
+        </div>
+      </div>
     </>
   );
 };
